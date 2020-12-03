652673065d4760be99442a06c49c73521bb860d4
Ubuntu <ubuntu@ip-172-31-76-11.ec2.internal>
2020-12-03 17:31:39 +0000

    This is a guessing game

---
+++ constants.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+
+# Created by Larry Nkengbeza
+# Created on December 2020
+# This module contains constant for guessing_game_program
+
+COMPUTER_GUESS = 9
--- guessing_game_program.py
+++ guessing_game_program.py
@@ -4,49 +4,27 @@
 # Created on December 2020
 # This program is a guessing game program
 
+import constants
+
 
 def main():
     # this funtion tells user if they guessed the right number
 
     # Input
-    random_guess = int(input("Enter a number between 0 to 9: "))
-    # Output
-    print("")
-    # process
-    if random_guess == 9:
-        # Output
-        print("Correct! Good Job!")
+    user_guess = int(input("Enter a number between 0 to 9: "))
+
     # Process
-    if random_guess == 5:
+    if user_guess == constants.COMPUTER_GUESS:
         # Output
-        print("Wrong!")
-    # Process
-    if random_guess == 1:
+        print("Good guess you got it right")
+        # Process
+    if user_guess > constants.COMPUTER_GUESS:
         # Output
-        print("Wrong!")
-    # Process
-    if random_guess == 3:
+        print("Wrong, the answer you gave is incorrect")
+        # Process
+    if user_guess < constants.COMPUTER_GUESS:
         # Output
-        print("Wrong!")
-    # Process
-    if random_guess == 4:
-        # Output
-        print("Wrong!")
-    # Process
-    if random_guess == 8:
-        # Output
-        print("Wrong!")
-    # Process
-    if random_guess == 7:
-        # Output
-        print("Wrong!")
-    if random_guess == 2:
-        # Output
-        print("Wrong!")
-    # Process
-    if random_guess == 6:
-        # Output
-        print("Wrong!")
+        print("Wrong, the answer you gave is incorrect")
 
 
 if __name__ == "__main__":
